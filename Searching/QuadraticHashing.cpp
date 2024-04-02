05c70e4843b9086488d7fc9f39dc2a2d8da4af90
Samir1611 <123559550+Samir1611@users.noreply.github.com>
2024-04-03 01:50:57 +0545

    Hashing

---
+++ Searching/QuadraticHashing.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cout<<"Enter the size of array elements";
+	cin>>n;
+
+	int values[n];
+	bool taken[n];
+	int a[n];
+	int prob2 = 0;
+
+		for(int i = 0; i<n; i++){
+			cout<<"Enter the element for position "<<i+1<<" ";
+			cin>>a[i];
+		}
+		cout << "function h(x) = x MOD "<<n<<" using linear probing" << endl;
+        cout << "Position\tValue\t\tProb" << endl;
+   
+        for (int i = 0; i < n; i++){
+        taken[i] = false;
+        }
+        for (int i = 0; i < n; i++) {
+        int position = a[i] % n;
+        if (!taken[position]) {				//true not taken
+            values[position] = a[i];
+            prob2=1;
+        } else {
+             int offset = 1;
+            while (taken[position]) {
+                position = (position + offset*offset) % n;
+            }
+                offset++;
+                prob2++;
+                values[position] = a[i];
+        }
+            taken[position] = true;
+     cout << "   " << position << "\t\t  " << values[position] << "\t\t" << prob2 << endl;
+    }
+    return 0;
+}
