5c5261d0c595fbc1854f2bd70228745f2e17134b
Samir1611 <123559550+Samir1611@users.noreply.github.com>
2024-04-03 16:00:27 +0545

    Doesn't Include message, if position not found garbage val occurs

--- Searching/QuadraticHashing.cpp
+++ Searching/QuadraticHashing.cpp
@@ -4,41 +4,43 @@ using namespace std;
 int main() {
 	
 	int n;
-	cout<<"Enter the size of array elements";
-	cin>>n;
+	cout << "Enter the size of array elements: ";
+	cin >> n;
 
 	int values[n];
 	bool taken[n];
 	int a[n];
 	int prob2 = 0;
 
-		for(int i = 0; i<n; i++){
-			cout<<"Enter the element for position "<<i+1<<" ";
-			cin>>a[i];
-		}
-		cout << "function h(x) = x MOD "<<n<<" using linear probing" << endl;
-        cout << "Position\tValue\t\tProb" << endl;
+	for(int i = 0; i < n; i++){
+		cout << "Enter the element for position " << i+1 << ": ";
+		cin >> a[i];
+	}
+	
+	cout << "Function h(x) = x MOD " << n << " using quadratic probing" << endl;
+    cout << "Position\tValue\t\tProb" << endl;
    
-        for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         taken[i] = false;
-        }
-        for (int i = 0; i < n; i++) {
+    }
+
+    for (int i = 0; i < n; i++) {
         int position = a[i] % n;
-        if (!taken[position]) {				//true not taken
+        prob2 = 1; 
+        if (!taken[position]) {	// True if not taken
             values[position] = a[i];
-            prob2=1;
         } else {
-             int offset = 1;
+            int offset = 1;
+            int original_position = position; 
             while (taken[position]) {
-                position = (position + offset*offset) % n;
-		      offset++;
+                position = (original_position + offset * offset) % n; 
+                offset++;
                 prob2++;
             }
-              
-                values[position] = a[i];
+            values[position] = a[i];
         }
-            taken[position] = true;
-     cout << "   " << position << "\t\t  " << values[position] << "\t\t" << prob2 << endl;
+        taken[position] = true;
+        cout << "   " << position << "\t\t  " << values[position] << "\t\t" << prob2 << endl;
     }
     return 0;
 }
